5b56775a647bd65276a766c2136d9319ec80c771
Fonysony <wmdragonfire@gmail.com>
2023-04-13 20:40:21 -0500

    Added itemAverage

---
+++ itemAverage/itemAverage.c
@@ -0,0 +1,19 @@
+struct Product {
+	int weight;
+	int numberOfItems;
+};
+
+void itemWeightAverage(struct Product item1, struct Product item2) {
+
+}
+
+int main()
+{
+	struct Product item1;
+	item1.weight = 15;
+	item1.numberOfItems = 5;
+
+	struct Product item2;
+	item2.weight = 25;
+	item2.numberOfItems = 4;
+}
