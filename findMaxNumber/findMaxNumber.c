f76cd440b282d61a71a5ffe8b39a18991fa8654b
Fonysony <wmdragonfire@gmail.com>
2023-10-16 09:34:51 -0500

    Working on findMaxNumber.c

--- findMaxNumber/findMaxNumber.c
+++ findMaxNumber/findMaxNumber.c
@@ -2,8 +2,14 @@
 
 void maxNumbers(int arr[], int arrSize)
 {
+   // Find the max number by going through each and comparing and replacing at the first spot
+   int max = arr[0];
    for (int i = 0; i < arrSize; i++) {
-      printf("%d\n", arr[i]);
+      for (int j = i; j < arrSize; j++) {
+         if (max < arr[j]) {
+            printf("%d\n", arr[j]);
+         }
+      }
    }
 }
 
